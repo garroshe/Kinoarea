2b3eee7b371f3ae79aed5042c66c6abda8f1859a
Taras <taraslebedunsky@gmail.com>
2025-07-08 19:55:00 +0300

    fix(ka-015): fix bug with build

---
+++ src/shared/types/vite-env.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_BEARER_TOKEN: string;
+  readonly VITE_API_KEY: string;
+
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_DATABASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
