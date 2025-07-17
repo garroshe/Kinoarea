3a70f9fcb837e043e55802cb8585dc06dcd56449
Taras <taraslebedunsky@gmail.com>
2025-07-17 19:50:59 +0300

    feat(ka-018): add privicy and deletion page to auth facebook

---
+++ src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -0,0 +1,13 @@
+const PrivacyPolicy = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Політика конфіденційності</h1>
+      <p>
+        Ми не зберігаємо особисті дані користувачів. Ваші дані використовуються виключно для автентифікації через
+        Facebook.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
