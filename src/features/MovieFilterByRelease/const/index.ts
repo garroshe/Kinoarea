9be312e4737e1816e2077a13c248a2cb9ec0fe71
Taras <taraslebedunsky@gmail.com>
2026-01-20 15:05:43 +0200

    feat(ka-054): remove hardcode in popular movies

--- src/features/MovieFilterByRelease/const/index.ts
+++ src/features/MovieFilterByRelease/const/index.ts
@@ -1,9 +1,14 @@
+const CURRENT_YEAR = new Date().getFullYear();
+const YEARS_BACK = 5;
+
 export const LIST_ITEMS_BY_RELEASE = [
   { label: "Весь час", date: null, key: 1 },
-  { label: "2025", date: 2025, key: 2 },
-  { label: "2024", date: 2024, key: 3 },
-  { label: "2023", date: 2023, key: 4 },
-  { label: "2022", date: 2022, key: 5 },
-  { label: "2021", date: 2021, key: 6 },
-  { label: "2020", date: 2020, key: 7 },
+  ...Array.from({ length: YEARS_BACK + 1 }, (_, i) => {
+    const year = CURRENT_YEAR - i;
+    return {
+      label: year.toString(),
+      date: year,
+      key: i + 2,
+    };
+  }),
 ];
